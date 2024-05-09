018ed085621aef03797832d858d5306a2742411e
Jose Naime <josep.naimeg@gmail.com>
2024-05-08 20:59:37 -0600

    feat: Design and user context prompt added

--- background.js
+++ background.js
@@ -1,36 +1,40 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Message received: ", message);
     if (message.action === "capture_screenshot") {
-        chrome.tabs.captureVisibleTab(null, { format: "png" }, (image) => {
+        chrome.tabs.captureVisibleTab(null, { format: "png" }, (base64_image) => {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError.message);
+                console.error("Error capturing screenshot: ", chrome.runtime.lastError.message);
                 sendResponse({ success: false, error: chrome.runtime.lastError.message });
             } else {
-                sendResponse({ success: true, screenshot: image });
+                console.log("Screenshot captured: ", base64_image);
+                sendResponse({ success: true, base64_image: base64_image });
             }
         });
-        return true; // Indicates that the response will be sent asynchronously
-    } else if (message.action === "analyze_image") { // Image is sent to the server for analysis
-        /*const url = "http://localhost:5000/analyze";
+
+        return true; // Asynchronous response
+
+    } else if (message.action === "analyze_image") {
+        const { base64_image, site_context } = message;
+        const url = "http://127.0.0.1:5000/api/v1/get_test_cases";
+
+        console.log("sending image to server: ", base64_image);
+
         fetch(url, {
             method: "POST",
-            body: JSON.stringify({ image: message.image }),
             headers: {
                 "Content-Type": "application/json"
-            }
+            },
+            body: JSON.stringify({ base64_image: toString(base64_image), site_context: toString(site_context)}),
+
         }).then(response => response.json()).then(data => {
+            console.log("Data: ", data);
             sendResponse({ success: true, data });
         }).catch(error => {
             console.error("Error: ", error);
-            sendResponse({ success: false, error });
-        });*/
-        // TODO: Send image to the server and retrieve the response
-        console.log("Image sending: ", message.image);
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-            sendResponse({ success: false, error: chrome.runtime.lastError.message });
-        } else {
-            sendResponse({ success: true });
-        }
-        return true; // Indicates that the response will be sent asynchronously
+            sendResponse({ success: false, error: error.message });
+        });
+
+        return true; // Asynchronous response
     }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,55 @@
-// Sends the image to the server to be analyzed
-function sendImageToAnalyze(screenshot){
-    chrome.runtime.sendMessage({ action: "analyze_image", image: screenshot }, (response) => {
-        if (response.success) {
-            // TODO: Handle success response from the server
-            console.log("Image was sent ");
-        } else {
-            console.error("Screenshot capture failed: " + response.error);
-        }
-    });
-}
+let loadingIcon = document.getElementById("loading-icon");
+let screenshotButton = document.getElementById("screenshot-button");
 
 // Triggers the capture of a screenshot
 document.getElementById("screenshot-button").addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "capture_screenshot" }, (response) => {
+    chrome.runtime.sendMessage({action: "capture_screenshot"}, (response) => {
+        console.log("sendImageToAnalyze response: ", response)
         if (response.success) {
-            const link = document.createElement("a");
-            const screenshot = response.screenshot;
-            link.href = screenshot;
-            link.download = "screenshot.png";
-            sendImageToAnalyze(screenshot);
-            //link.click();
+            console.log("Entered screennshot-button success block")
+            const base64_image = response.base64_image;
+            console.log("Screenshot captured: ", base64_image);
+
+            // get textarea value
+            const site_context = document.getElementById("screenshot-context").value;
+
+            screenshotButton.style.visibility = "hidden";
+            loadingIcon.style.display = "block";
+
+            sendImageToAnalyze(base64_image, site_context);
         } else {
             console.error("Screenshot capture failed: " + response.error);
         }
     });
 });
+
+// Sends the image to the server to be analyzed
+function sendImageToAnalyze(base64_image, site_context) {
+    chrome.runtime.sendMessage({
+        action: "analyze_image",
+        base64_image: base64_image,
+        site_context: site_context
+    }, (response) => {
+        console.log("sendImageToAnalyze response: ", response)
+        if (response.success) {
+            console.log("Entered analyze_image success block")
+            // TODO: Handle success response from the server
+            // Open new tab with the result
+            document.getElementById("test-cases-list").innerHTML = response.data;
+            document.getElementsByTagName("html")[0].classList.add("expand");
+
+            loadingIcon.style.display = "none";
+
+            console.log("Response from server: ", response);
+
+        } else {
+            console.error("Screenshot capture failed: " + response.error);
+            loadingIcon.style.display = "none";
+        }
+
+
+        return true;
+    });
+}
+
+
