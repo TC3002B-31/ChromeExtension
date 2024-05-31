d7ac1be5fc314fff0a9375da8e720fb291fe81c4
kreiji <80983436+kreiji@users.noreply.github.com>
2024-05-30 18:03:53 -0600

    Added styles and functionality to downloaded HTML

--- popup/popup.js
+++ popup/popup.js
@@ -89,15 +89,160 @@ document.getElementById('download-button').addEventListener('click', function ()
     // Generate the HTML content
     let testCasesListContent = document.getElementById('test-cases-list').innerHTML;
     let progressContainerContent = document.getElementById('progress-container').innerHTML;
+    let site_context = document.getElementById("screenshot-context");
 
     let htmlContent = `
     <html>
     <head>
         <style>
+            html {
+                background: #161618;
+            }
+
+            body {
+                // margin: 14px;
+                display: flex;
+                justify-content: center;
+                font-family: Arial, sans-serif;
+            }
+
+            h1 {
+                color: var(--primary-text-color);
+                font-size: 1.8em;
+                text-decoration: underline;
+                margin: 0;
+                padding: 20px 15px;
+                text-align: center;
+            }
+
+            li {
+                list-style-type: none;
+            }
+
+            .list-item {
+                display: grid;
+                grid-template-columns: 1em auto;
+                gap: 0.8em;
+                margin-left: 2em;
+            }
+
+            #content-container {
+                color: #FFF;
+                background-color: #242529;
+                width: 50%;
+                overflow: auto;
+            }
+
+            #content-container::-webkit-scrollbar {
+                width: 10px;
+            }
+
+            #context-container {
+                display        : flex;
+                gap            : 10px;
+                flex-direction : column;
+                padding        : 10px;
+                text-align: center; 
+                justiy-content: center;
+            }
+
+            #context-container p {
+                color: #838486;
+            }
+ 
+            #progress-container {
+                position: fixed;
+                top: 0;
+                right: 20.5%;
+                padding: 10px;
+                transform: translateX(-55%);
+                background: rgba(0, 0, 0, 0.25);
+                backdrop-filter: blur(5px);
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                color: #1D7FFF;
+            }
+
+            #progress-container aside {
+                color: #838486;
+                font-style: italic;
+            }
+
+            #progress-value {
+                margin-top: 0;
+                margin-bottom: 4px;
+                font-size: 1.5rem;
+                font-weight: bold;
+            }
+
+            #test-cases-list {
+                padding-bottom: 15px;
+                display: flex;
+                flex-direction: column;
+                gap: 0.5em;
+                margin: 1em;
+            }
+
+            .relative {
+                position: relative;
+            }
+
+            .list-item {
+                display               : grid;
+                grid-template-columns : 1em auto;
+                gap                   : 0.8em;
+                margin-left: 2em;
+            }
+            
+            input[type='checkbox'] {
+                -webkit-appearance : none;
+                appearance         : none;
+                background-color   : #242529;
+                margin             : 0;
+                font               : inherit;
+                color              : #838486;
+                width              : 1.15em;
+                height             : 1.15em;
+                border             : 0.1em solid currentColor;
+                border-radius      : 0.15em;
+                transform          : translateY(-0.075em);
+                display            : grid;
+                place-content      : center;
+            }
+            
+            input[type='checkbox']::before {
+                content          : '';
+                width            : 0.65em;
+                height           : 0.65em;
+                clip-path        : polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);
+                transform        : scale(0);
+                transform-origin : bottom left;
+                transition       : 120ms transform ease-in-out;
+                box-shadow       : inset 0.5em 0.5em #1D7FFF;
+                /* Windows High Contrast Mode */
+                background-color : CanvasText;
+            }
+            
+            input[type='checkbox']:checked::before {
+                color: #1D7FFF;
+                border             : 0.1em solid currentColor;
+                transform : scale(1);
+            }
+            
+            input[type='checkbox']:focus {
+                outline        : max(2px, 0.15em) solid currentColor;
+                outline-offset : max(2px, 0.15em);
+            }
         </style>
     </head>
     <body>
-        <div class="relative">
+        <div id="content-container" class="relative">
+            <h1>SiteProof</h1>
+            <div id="context-container">
+                <p>Context: ${site_context.value}</p>
+            </div>
             <div id="progress-container">
                 ${progressContainerContent}
             </div>
